f35900a8c985667d19452a4032995169df78e9cf
sataverse <odrimung2@gmail.com>
2024-06-27 19:35:29 +0900

    Modify ScoreRect

--- src/app/_component/post/ScoreRects.tsx
+++ src/app/_component/post/ScoreRects.tsx
@@ -11,19 +11,19 @@ export default function ScoreRects({ text, score }: Props) {
       <div>{text}</div>
       <div>
         <div>
-          <div style={{ width: score === 1 ? '19rem' : score > 1 ? '37rem' : '0' }} />
+          <div style={{ width: score === 1 ? '20rem' : score > 1 ? '38rem' : '0' }} />
         </div>
         <div>
-          <div style={{ width: score === 3 ? '19rem' : score > 3 ? '37rem' : '0' }} />
+          <div style={{ width: score === 3 ? '20rem' : score > 3 ? '38rem' : '0' }} />
         </div>
         <div>
-          <div style={{ width: score === 5 ? '19rem' : score > 5 ? '37rem' : '0' }} />
+          <div style={{ width: score === 5 ? '20rem' : score > 5 ? '38rem' : '0' }} />
         </div>
         <div>
-          <div style={{ width: score === 7 ? '19rem' : score > 7 ? '37rem' : '0' }} />
+          <div style={{ width: score === 7 ? '20rem' : score > 7 ? '38rem' : '0' }} />
         </div>
         <div>
-          <div style={{ width: score === 9 ? '19rem' : score > 9 ? '37rem' : '0' }} />
+          <div style={{ width: score === 9 ? '20rem' : score > 9 ? '38rem' : '0' }} />
         </div>
       </div>
     </div>
